93950864709a0d5502ec81c5f7c08d9b6aa3507f
Alexander Schwartzberg <aeksco@gmail.com>
2024-01-24 16:56:34 -0800

    Cleanup setup.sh

--- setup.sh
+++ setup.sh
@@ -13,21 +13,6 @@ git clone https://github.com/zsh-users/zsh-history-substring-search
 # TMUX Ressursct
 git clone https://github.com/tmux-plugins/tmux-resurrect ./tmux/plugins/tmux-resurrect
 
-# Copies .zshrc, .zshenv
-# cp ~/dotfiles/zshrc ~/.zshrc
-# cp ~/dotfiles/zshenv ~/.zshenv
-
-# Copies .vimrc
-# cp ~/dotfiles/vimrc ~/.vimrc
-
-# Copies .gitconfig.local, .gitconfig, .gitmessage
-# cp ~/dotfiles/gitconfig.local ~/.gitconfig.local
-# cp ~/dotfiles/gitconfig ~/.gitconfig
-# cp ~/dotfiles/gitmessage ~/.gitmessage
-
-# Copies .tmux.conf
-# cp ~/dotfiles/tmux.conf ~/.tmux.conf
-
 # # # # 
 # PGP Setup
 # https://gist.github.com/troyfontaine/18c9146295168ee9ca2b30c00bd1b41e
